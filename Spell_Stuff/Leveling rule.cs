5a8270c9e1936daa2724ddc5bc20ddb3a8b7abbd
Chomper-afk <Marko.mimi10000@gmail.com>
2025-04-22 13:10:56 +0200

    quick fix

--- Spell_Stuff/Leveling rule.cs
+++ Spell_Stuff/Leveling rule.cs
@@ -17,7 +17,7 @@ namespace DnD_Battle.Spell_Stuff {
 
         public Leveling_rule(int _Times = 0, int[]? _Times_Steps = null, DMG? _DMG = null, int[]? _DMGSteps = null, DMG? _SpellSlot = null, int _SSSteps = 0) {
             Times = _Times;
-            Times_Steps.Clear();
+            Times_Steps = new List<int>();
             if(_Times_Steps != null) {
                 foreach(int _Step in _Times_Steps) {
                     Times_Steps.Add(_Step);
@@ -25,7 +25,7 @@ namespace DnD_Battle.Spell_Stuff {
                 }
             DMGPerLevelStep = _DMG ?? new DMG();
 
-            DMG_Steps.Clear();
+            DMG_Steps = new List<int>();
             if(_DMGSteps != null) {
                 foreach (int _Step in _DMGSteps) { DMG_Steps.Add(_Step); }
                 }
